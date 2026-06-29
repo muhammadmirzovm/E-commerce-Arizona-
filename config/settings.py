9c0aa98cda264e5aaca1d6127f688447c0d1e2e2
mukhammadmirzo <mukhammadmirzo078@gmail.com>
2026-06-29 17:03:30 +0500

    added checkout

--- config/settings.py
+++ config/settings.py
@@ -41,7 +41,8 @@ INSTALLED_APPS = [
     'crispy_forms',
     'crispy_bootstrap5',
     'catalog',
-    'cart'
+    'cart',
+    'orders',
 ]
 
 MIDDLEWARE = [
--- config/urls.py
+++ config/urls.py
@@ -25,6 +25,7 @@ urlpatterns = [
     path('accounts/', include('accounts.urls')),
     path("", include("catalog.urls")),
     path("", include("cart.urls")),
+    path("", include("orders.urls")),
 ]
 
 if settings.DEBUG:
